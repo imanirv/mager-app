06632ff3ff6dab2568916443ed0486996c40305c
imanirv <imanirvansyaah@gmail.com>
2022-03-30 20:11:29 +0700

    update project template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import HomeContainer from '../containers/home'
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen bg-slate-900 text-white flex flex-col items-center justify-center'>
-      <h1 className='text-3xl'>Welcome to Mager App</h1>
-      <p className='mt-10'>ini template buat mager app, nanti abis clone langsung npm install dlu biar keinstal depedencies nya</p>
-    </div>
+    <HomeContainer />
   )
 }
