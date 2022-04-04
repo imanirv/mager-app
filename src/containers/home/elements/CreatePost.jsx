6279623cb6ac471702030cb833627a5cff1ab5e2
imanirv <imanirvansyaah@gmail.com>
2022-04-04 16:45:19 +0700

    feat/fp-60: update icon, pop up

--- src/containers/home/elements/CreatePost.jsx
+++ src/containers/home/elements/CreatePost.jsx
@@ -1,6 +1,81 @@
 
 import { PhotographIcon, PlayIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import LiveIcon from "../../../../public/custom-icon/live"
+
+
+const MyModal = ({status, close}) => {
+  return (
+    <>
+      <Transition appear show={status} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={close}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-[800px] h-40  p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-darkmode-2 border border-gray-200 shadow-xl rounded-2xl">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-white"
+                >
+                  Payment successful
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-white">
+                    Your payment has been successfully submitted. We’ve sent you
+                    an email with all of the details of your order.
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={close}
+                  >
+                    Got it, thanks!
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
+
 
 const CreatePostItem = ({children}) => (
     <div className={`bg-darkmode-3 hover:bg-darkmode-hover text-gray-200 h-10 w-12  mr-2 rounded-lg flex items-center justify-center`}>
@@ -8,16 +83,32 @@ const CreatePostItem = ({children}) => (
     </div>
 )
 
-const CreatePost = () => (
-    <div className={`bg-darkmode-2 w-full rounded-2xl px-3 py-4 flex items-center justify-center`}>
-    <CreatePostItem>
-        <Image src={"/images/profile.png"} width={50} height={50} alt="profile"/>
-    </CreatePostItem>
-    <div className={`bg-darkmode-3 hover:bg-darkmode-hover text-gray-200 w-full h-10 rounded-lg mx-2 flex items-center px-4`}>Buat post</div>
-    <CreatePostItem><PhotographIcon className={`bg-darkmode-3 w-4 h-4`} /></CreatePostItem>
-    <CreatePostItem><PlayIcon className={`bg-darkmode-3 w-4 h-4`} /></CreatePostItem>
-</div>
-)
+const CreatePost = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const closeModal= () => {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+    // console.log(isOpen)
+  }
+
+    return(
+        <>
+        <div className={`bg-darkmode-2 w-full rounded-2xl px-3 py-4 flex items-center justify-center`}>
+            <CreatePostItem>
+                <Image src={"/images/profile.png"} width={50} height={50} alt="profile"/>
+            </CreatePostItem>
+            <div className={`bg-darkmode-3 hover:bg-darkmode-hover text-gray-200 w-full h-10 rounded-lg mx-2 flex items-center px-4` }  onClick={openModal}>Buat post</div>
+            <CreatePostItem><PhotographIcon className={`bg-darkmode-3 w-4 h-4`} /></CreatePostItem>
+            <CreatePostItem><LiveIcon /></CreatePostItem>
+        </div>
+        <MyModal status={isOpen} close={closeModal}/>
+        </>
+    )
+}
+
 
 
 export default CreatePost
