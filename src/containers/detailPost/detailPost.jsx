4eaf45e35cda925c1d1e02257f4145add436cac1
imanirv <imanirvansyaah@gmail.com>
2022-04-11 14:51:02 +0700

    feat/fp-38: merge navbar to development

--- src/containers/detailPost/detailPost.jsx
+++ src/containers/detailPost/detailPost.jsx
@@ -1,6 +1,28 @@
+import MainLayout from "../../components/layout/"
+import CommunityCard from "../../components/elements/CommunityCard"
+
+const PostItem = () => {
+    return (
+        <div className=""></div>
+    )
+}
+
 const DetailPostContainer = () => {
     return (
-        <h1>hello detail </h1>
+        <MainLayout>
+             <div className="pt-4 px-3 lg:px-40">
+                <div className="flex items-start justify-center">
+                    <div className=" w-full md:w-8/12 mr-3">
+
+                    </div>
+                    <div className="hidden md:block w-4/12  bg-darkmode-2 rounded-2xl">
+                        {/* card komunitas taro sini */}
+                        <CommunityCard />
+                    </div>
+                
+                </div>
+            </div>
+        </MainLayout>
     )
 }
 
