a333905256b731cdceededd4e48dcb6ce08654a9
imanirv <imanirvansyaah@gmail.com>
2022-05-23 09:54:59 +0700

    feat/fp-141: follows page default index

---
+++ src/pages/user/[idUser]/followers.js
@@ -0,0 +1,12 @@
+
+import FollowsContainer from "../../../containers/follow"
+
+
+const FollowerPage = () => {
+    return (
+        <FollowsContainer index={0} />
+    )
+    
+}
+
+export default FollowerPage
---
+++ src/pages/user/[idUser]/following.js
@@ -0,0 +1,12 @@
+
+import FollowsContainer from "../../../containers/follow"
+
+
+const FollowerPage = () => {
+    return (
+        <FollowsContainer index={1} />
+    )
+    
+}
+
+export default FollowerPage
